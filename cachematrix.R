9ac294f5a5f844a3e9aa585e3c5d2440c8216638
raninooraeni <63456261+raninooraeni@users.noreply.github.com>
2020-06-06 19:49:19 +0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) {
+    if (det(x)==0) {
+      stop("det matrix = 0 can not be inverted")
+    }
+    if (dim(x)[1]!=dim(x)[2]) {
+      stop("matrix must be square")
+    }
+    s <<- solve
+  }
+    
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -12,4 +31,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
